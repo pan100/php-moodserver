6460e998bcc099962e86a5c164a89dcfc3efbec7
Per-Øivin Berg Andersen <pan100@moodcartservervirtual.(none)>
2013-02-26 14:14:22 +0100

    added entity Group for fos_bundle. Will maybe edit later if I need a name for it or something. The entity only has an auto integer as ID.

---
+++ src/Pan100/MoodLogBundle/Entity/Group.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pan100\MoodLogBundle\Entity;
+
+use FOS\UserBundle\Entity\Group as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+     protected $id;
+}
