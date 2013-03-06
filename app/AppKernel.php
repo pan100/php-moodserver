4f9b3d64edd3ba7514f5494706c3c5c39993794c
Per-Øivin Berg Andersen <post@perandersen.no>
2013-03-06 19:23:56 +0100

    added intl sonata and outputted dates on user in report view.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Pan100\MoodLogBundle\Pan100MoodLogBundle(),
+            new Sonata\IntlBundle\SonataIntlBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
