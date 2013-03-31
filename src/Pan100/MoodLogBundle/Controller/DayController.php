b59e0849f4ad2e019999a8c14834f684a53bb3f0
Per-Øivin Berg Andersen <post@perandersen.no>
2013-03-31 20:16:12 +0200

    created a form for posting, but nothing is done for now. Yet to get the data into the form based on the datepicker and posting data and parsing it.

--- src/Pan100/MoodLogBundle/Controller/DayController.php
+++ src/Pan100/MoodLogBundle/Controller/DayController.php
@@ -4,33 +4,25 @@ namespace Pan100\MoodLogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Pan100\MoodLogBundle\Entity\Day;
 
 class DayController extends Controller
 {
-	public function indexAction(Request $request, $date) {
-		
-	}
+	public function postAction(Request $request) {
+		if($this->getUser()->hasRole('ROLE_PATIENT')) {
+			//render the form
+			$day = new Day();
+			$day->setDate(new \DateTime);
+			$day->setSleepHours(8);
 
-	public function postAction(Request $request, $date) {
-		if($request->getRequestFormat() == 'html') {
-			if($this->getUser->hasRole('ROLE_PATIENT')) {
-				//render the form
-				
-				//handle submissions, validate the data and create a new date object or update an existing one			
-			}
+	        $form = $this->createFormBuilder($day)
+            ->add('date', 'date')->add('sleepHours', 'integer')
+            ->getForm();
+
+	        return $this->render('Pan100MoodLogBundle:Day:post.html.twig', array(
+	            'form' => $form->createView()));
+			//handle submissions, validate the data and create a new date object or update an existing one			
 		}
-		elseif($request->getRequestFormat() == 'json') {
-			//check if the request is containing a username and password field, if not report that the request was incomplete
-			//if authentication is ok, validate the data and create a new date object or update an existing one
-		}
-		else {
-			//return a bad request code
-		}
-
+		throw new \Exception('You are not a patient and cannot access this URI');
 	}
-
-	public function deleteAction(Request $request, $date) {
-
-	}
-
 }
