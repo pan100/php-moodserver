a7a521ade4b66108f49617ee7ce3837e697bf3c7
Per-Øivin Berg Andersen <post@perandersen.no>
2013-06-24 23:45:27 +0200

    setting key-value to false in json_decode

--- src/Pan100/MoodLogBundle/Controller/DayController.php
+++ src/Pan100/MoodLogBundle/Controller/DayController.php
@@ -36,7 +36,7 @@ class DayController extends Controller
 		$content = $this->get("request")->getContent();
 		if (!empty($content))
 		{
-    		$params = json_decode($content, true, 3); // 2nd param to get as array
+    		$params = json_decode($content, false, 3); // 2nd param to get as array
     		if($params == null) {
     			$logger->err("params was empty - " . \Doctrine\Common\Util\Debug::dump($params));
     			return new Response("Feil i forespørsel", 400);
